76ef925e495b677f36fdfdb7f30d6873744533c5
Abdullah Hamza <abhamza213@gmail.com>
2022-03-23 15:40:59 +0200

    Added login API

--- profile_api/views.py
+++ profile_api/views.py
@@ -1,12 +1,14 @@
-from email import message
-from lib2to3.pgen2 import token
-from urllib import response
+# from email import message
+# from lib2to3.pgen2 import token
+# from urllib import response
 from rest_framework.views import APIView
 from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.authentication import TokenAuthentication
 from rest_framework import filters
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 
 from profile_api import serializers
 from profile_api import models
@@ -111,4 +113,8 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     
 
       
+class UserLoginApiView(ObtainAuthToken):
+    """Handle creating user authentication tokens"""
+    renderer_classes= api_settings.DEFAULT_RENDERER_CLASSES
+
         
